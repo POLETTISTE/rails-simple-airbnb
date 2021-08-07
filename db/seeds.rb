0c431ebf3259f567407a1082e1ea3883b72a0358
STEPHANE POLETTI <polettistephane@msn.com>
2021-08-07 23:03:59 +0200

    Add photo to Flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+flats = Flat.create
+
+Formule1 = {name: " Formule1", address: "ZAC brignoles 83 Brignoles", description: "Hôtel pas cher où tu dors pas bien", price_per_night: 22, number_of_guests: 2 }
+Terresrouges = { name: "Les Terres Rouges", address: "Les terres rouges quartier le Tuf 83170 Tourves", description: "Best place ever to sleep, lovely family here", price_per_night: 0, number_of_guests: 10 }
+
+[ Formule1, Terresrouges ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+
+
+puts 'Creating 100 fake flats...'
+100.times do
+  flat = Flat.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::String.random,
+    price_per_night:  rand(0..100),
+    number_of_guests: rand(1..5)
+  )
+  flat.save!
+end
+puts 'Finished!'
