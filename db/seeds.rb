eb2e89e93e16a998c5799a6071308a3d6470fa3c
STEPHANE POLETTI <polettistephane@msn.com>
2021-08-08 22:49:57 +0200

    resolution loop link to www picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@ puts "Cleaning database..."
 Flat.destroy_all
 
 puts "Creating flats..."
-flats = Flat.create
 
-Formule1 = {name: " Formule1", address: "ZAC brignoles 83 Brignoles", description: "Hôtel pas cher où tu dors pas bien", price_per_night: 22, number_of_guests: 2 }
-Terresrouges = { name: "Les Terres Rouges", address: "Les terres rouges quartier le Tuf 83170 Tourves", description: "Best place ever to sleep, lovely family here", price_per_night: 0, number_of_guests: 10 }
 
-[ Formule1, Terresrouges ].each do |attributes|
+flat1 = {name: " Formule1", address: "ZAC brignoles 83 Brignoles", description: "Hôtel pas cher où tu dors pas bien", price_per_night: 22, number_of_guests: 2, photo: "https://picardie.media.tourinsoft.eu/upload/HOTPIC080FS00007-Formule-1-ext-Glisy-Somme-Picardie.jpg?width=1075&height=736&crop=1"}
+flat2 = { name: "Les Terres Rouges", address: "Les terres rouges quartier le Tuf 83170 Tourves", description: "Best place ever to sleep, lovely family here", price_per_night: 0, number_of_guests: 10, photo: "https://www.izoa.fr/3806-large_default/tableau-contemporain-terre-rouge.jpg"}
+
+
+[ flat1, flat2 ].each do |attributes|
   flat = Flat.create!(attributes)
   puts "Created #{flat.name}"
 end
@@ -23,8 +24,10 @@ puts 'Creating 100 fake flats...'
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     description: Faker::String.random,
     price_per_night:  rand(0..100),
-    number_of_guests: rand(1..5)
+    number_of_guests: rand(1..5),
+    photo: Faker::Avatar.image
   )
   flat.save!
 end
 puts 'Finished!'
+
